cd1a2ca3f0218b41b3fb162a926e550bce61e742
dsafiulin <dsafiulin@test.ru>
2018-06-11 01:50:58 +0300

    Наследование Figure -> HorizontalLine; VerticalLine

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,20 +9,21 @@ namespace Snake
     class Program
     {
         static void Main(string[] args)
-        { 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+        {
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
-            hLine.Draw();
-
-            VerticalLine vLine = new VerticalLine(10, 5, 6, '#');
-            vLine.Draw();
-
-            Console.ReadLine();
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
         }
     }
 }
