2f31feaec1d1fdc66e8c328dff1ad15fb8d0453e
dsafiulin <dsafiulin@test.ru>
2018-06-12 13:35:10 +0300

    еда для змейки

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -57,6 +57,11 @@ namespace Snake
             sym = ' ';
             Draw();
         }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
     }
 
 
